303cb5d4bbcf0e0fc863e138270ce589aa4b9ffa
haeunsong <jamy0225@naver.com>
2020-11-24 19:32:54 +0900

    input/button 설정

--- reactcode/src/Components/Screen/Visitor/PhoneAuth.js
+++ reactcode/src/Components/Screen/Visitor/PhoneAuth.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import Viewer from '../Admin/Auth/Login/Resource/Style/StMain'
+import InputViewer from '../Admin/Auth/Login/Resource/Style/StInput'
+import {LoginButtonViewer,SignupButtonViewer} from '../Admin/Auth/Login/Resource/Style/StButton'
 
 // checkin/phone
 // 외부 방문자 휴대전화 번호 인증 화면(외부 방문자 로그인)
 const PhoneAuth = () => {
     return (
         <div>
-            <form>
-                <input type="text" placeholder="휴대전화 번호 ('-'제외 번호만 입력)" />
-                <input className="sendPhoneNumBtn" type="submit" value="인증하기"/><br/>
-                <input type="text" placeholder="인증코드(8자리):" /><br/>
-                <input type ="text"placeholder="이름:" /><br/>
-                <input type="text" placeholder="소속:"/><br/>
-                <button className="form_submit_button" type="submit">확인</button>
-            </form>
+            <Viewer>
+                <InputViewer type="text" placeholder="휴대전화 번호 ('-'제외 번호만 입력)" />
+                <InputViewer className="sendPhoneNumBtn" type="submit" value="인증하기"/><br/>
+                <InputViewer type="text" placeholder="인증코드(8자리):" /><br/>
+                <InputViewer type ="text"placeholder="이름:" /><br/>
+                <InputViewer type="text" placeholder="소속:"/><br/>
+                <SignupButtonViewer className="form_submit_button" type="submit">확인</SignupButtonViewer>
+            </Viewer>
             
         </div>
     );
