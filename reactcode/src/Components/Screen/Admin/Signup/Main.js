405b47375c3381a5e823a0338062ce41450ef1c0
haeunsong <jamy0225@naver.com>
2021-01-13 14:34:12 +0900

    회원가입 api호출

--- reactcode/src/Components/Screen/Admin/Signup/Main.js
+++ reactcode/src/Components/Screen/Admin/Signup/Main.js
@@ -1,12 +1,12 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import {
   AdminSignupButton,
   CertifyEmailButton,
   BackButton
 } from "./Button";
 import { useInput, Input } from "./Input";
-import { Viewer,Title } from "./Resource/Style/StMain";
+import { Viewer, Title } from "./Resource/Style/StMain";
 // admin/signup
 const Main = ({ setClickView }) => {
   const emailUseInput = useInput("학내메일로 가입(***@office.skhu.ac.kr):");
@@ -16,9 +16,30 @@ const Main = ({ setClickView }) => {
   const userPwUseInput = useInput("사용할 PW 입력:");
   const checkUserPwUseInput = useInput("PW 확인:");
 
-  const onCertifyEmail = async(email,userName,major,userId,userPw) => {
+  useEffect(() => {
+    const requestOptions = {
+      method: "POST",
+      // stringify() : 객체 => JSON문자열로 변환
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: emailUseInput.value,
+        name: nameUseInput.value,
+        major: majorUseInput.value,
+        userid: userIdUseInput.value,
+        userpw: userPwUseInput.value
+      })
+    };
+    fetch('https://skhuspass.sleepy-owl.com/user/adminsignup', requestOptions)
+      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.ok) console.log("OK");
+      })
+  }, []);
+
+  //   /user/adminsignup 
+  const onCertifyEmail = async (email, userName, major, userId, userPw) => {
     alert("이메일로 인증링크를 전송하였습니다. 이메일을 확인해주세요.");
-    
+
 
   }
   const onAdminSignup = () => {
@@ -30,8 +51,8 @@ const Main = ({ setClickView }) => {
     onCertifyEmail();
   };
   const onAdminSignupClick = () => {
-    onAdminSignup(emailUseInput.value,nameUseInput.value,majorUseInput.value,
-      userIdUseInput.value,userPwUseInput.value);
+    onAdminSignup(emailUseInput.value, nameUseInput.value, majorUseInput.value,
+      userIdUseInput.value, userPwUseInput.value);
   }
 
   // useInput이 반환하는 것: placeholder,value,onChange
@@ -39,21 +60,21 @@ const Main = ({ setClickView }) => {
   // 기기에 ID,비번 저장해서 두번째부터는 바로 로그인.
   return (
     <>
-    <Viewer>
-      <Title>건물 관리자 회원가입</Title>
-      <Input type="email" {...emailUseInput} />
-      <CertifyEmailButton onClick={onCertifyEmailClick} />
-      <Input type="text" {...nameUseInput} />
-      <Input type="text" {...majorUseInput} />
-      <Input type="text" {...userIdUseInput} />
-      <Input type="password" {...userPwUseInput} />
-      <Input type="password" {...checkUserPwUseInput} />
+      <Viewer>
+        <Title>건물 관리자 회원가입</Title>
+        <Input type="email" {...emailUseInput} />
+        <CertifyEmailButton onClick={onCertifyEmailClick} />
+        <Input type="text" {...nameUseInput} />
+        <Input type="text" {...majorUseInput} />
+        <Input type="text" {...userIdUseInput} />
+        <Input type="password" {...userPwUseInput} />
+        <Input type="password" {...checkUserPwUseInput} />
 
-      <Link to="/admin/login" style={{textDecoration:'none', display:'grid'}}>
-      <AdminSignupButton onClick={onAdminSignupClick} />
-      <BackButton />
-      </Link>
-    </Viewer>
+        <Link to="/admin/login" style={{ textDecoration: 'none', display: 'grid' }}>
+          <AdminSignupButton onClick={onAdminSignupClick} />
+          <BackButton />
+        </Link>
+      </Viewer>
     </>
   );
 };
